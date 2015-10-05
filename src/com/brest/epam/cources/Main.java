d4eb926aa5113208f6226e170181332abb755511
aliaksandr_fedasiuk <aliaksandr_fedasiuk@epam.com>
2015-10-05 18:09:54 +0300

    fix

---
+++ src/com/brest/epam/cources/Main.java
@@ -0,0 +1,15 @@
+package com.brest.epam.cources;
+
+/**
+ * Created by juga on 2.10.15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(" test ");
+        i++;
+        System.out.println(" test " + i);
+    }
+
+}
